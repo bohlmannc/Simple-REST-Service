5070a43116238b144e6a5acd74b045e1d4154b93
bohlmannc <44445083+bohlmannc@users.noreply.github.com>
2019-09-19 23:46:52 -0400

    Main python file

---
+++ rest_service.py
@@ -0,0 +1,91 @@
+import json
+import redis
+from flask import Flask
+from abc import ABC, abstractmethod
+
+
+app = Flask(__name__)
+
+
+class MessageStore(ABC):
+
+    def __init__(self):
+        super().__init__()
+
+    @abstractmethod
+    def write_message(self, message):
+        pass
+
+    @abstractmethod
+    def word_count(self):
+        pass
+
+
+class RedisMessageStore(MessageStore):
+
+    def __init__(self, host, port, db):
+        super().__init__()
+        self.host = host
+        self.port = port
+        self.db = db
+        self.r = redis.StrictRedis(host, port, db)
+
+    def write_message(self, message):
+        key = message['id']
+        value = message['message']
+        if key not in self.r:
+            self.r.set(key, value)
+            prev_count = int(self.r.get('count'))
+            new_count = prev_count + len(value.split())
+            self.r.set('count', new_count)
+
+    def word_count(self):
+        return int(self.r.get('count'))
+
+
+class InMemoryMessageStore(MessageStore):
+
+    def __init__(self):
+        super().__init__()
+        self.m = {}
+        self.count = 0
+
+    def write_message(self, message):
+        if message['id'] not in self.m:
+            self.m[message['id']] = message['message']
+            self.count += len(message['message'].split())
+
+    def word_count(self):
+        return self.count
+
+
+r = RedisMessageStore(host='localhost', port=6379, db=0)
+r.write_message({"id": "092", "message": "try"})
+r.write_message({"id": "13", "message": "unlucky"})
+r.write_message({"id": "099", "message": "add two"})
+r.write_message({"id": "584", "message": "very very very long string"})
+r.write_message({"id": "099", "message": "add two"})
+
+
+wc = str(r.word_count())
+c = 'count = ' + wc
+
+
+@app.route("/")
+def main():
+    print(c)
+    return json.dumps(c)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
